106df48557dfd7f79762b85fdb59d1cb5ca83d42
Sobri Kamal <slier81@gmail.com>
2018-08-09 16:24:00 +0800

    Completed Level 1

---
+++ src/Task5.kt
@@ -0,0 +1,1 @@
+fun containsEven(collection: Collection<Int>): Boolean = collection.any { elem -> elem % 2 == 0 }
---
+++ src/Task8.kt
@@ -0,0 +1,18 @@
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+
+    val email = client?.personalInfo?.email
+
+    email?.let {
+        message?.let {
+            mailer.sendMessage(email, message)
+        }
+    }
+}
+
+class Client(val personalInfo: PersonalInfo?)
+
+class PersonalInfo(val email: String?)
+
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}

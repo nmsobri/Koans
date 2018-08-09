ac3952f3fe43ad678534e21384716d63a606addd
Sobri Kamal <slier81@gmail.com>
2018-08-09 17:20:14 +0800

    Finish Introduction Chapter

---
+++ src/Task11.kt
@@ -0,0 +1,15 @@
+import java.util.*
+import kotlin.Comparator
+
+fun getList(): List<Int> {
+
+    val arrayList = arrayListOf(1, 5, 2)
+
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(x: Int, y: Int): Int {
+            return y - x
+        }
+    })
+
+    return arrayList
+}
